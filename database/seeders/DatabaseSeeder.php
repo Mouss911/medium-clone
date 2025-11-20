94f5b6bed56d163b3907994932f8c5ee50117764
Moussa Diatta <diattamoussa010@gmail.com>
2025-11-20 21:15:47 +0000

    mise à jour du Seeder et Dashboard

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Category;
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
             'email' => 'test@example.com',
         ]);
+
+        $categories = [
+            'Technology',
+            'Health',
+            'Travel',
+            'Education',
+            'Lifestyle',
+            'Food',
+            'Finance',
+            'Entertainment',
+            'Sports',
+            'Business',
+            'Politics',
+            'Science',
+            'Environment',
+            'Culture',
+            'Fashion',
+        ];
+
+        foreach ($categories as $category) {
+            Category::create(['name' => $category]);
+        }
+
+        Post::factory(100)->create();
+
     }
 }
