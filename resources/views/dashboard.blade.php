caa9f5d18fbfde23856590e4da435ad0ca7dc830
Moussa Diatta <diattamoussa010@gmail.com>
2025-11-20 21:42:00 +0000

    refaire le Dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,24 +3,25 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900">
-                    <ul class="flex flex-wrap text-sm font-medium text-center text-body">
+                    <ul
+                        class="flex flex-wrap text-sm font-medium text-center text-body text-gray-500 dark:text-gray-400 justify-center">
                         <li class="me-2">
-                            <a href="#" class="inline-block px-4 py-2.5 text-white bg-brand rounded-base active"
+                            <a href="#" class="inline-block px-4 py-3 text-white bg-blue-500 rounded-lg active"
                                 aria-current="page">Tab 1</a>
                         </li>
                         <li class="me-2">
                             <a href="#"
-                                class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
                                 2</a>
                         </li>
                         <li class="me-2">
                             <a href="#"
-                                class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
                                 3</a>
                         </li>
                         <li class="me-2">
                             <a href="#"
-                                class="inline-block px-4 py-3 rounded-base hover:text-heading hover:bg-neutral-secondary-soft">Tab
+                                class="inline-block px-4 py-3 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white">Tab
                                 4</a>
                         </li>
                         <li>
